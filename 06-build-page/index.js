a42f1c8bbd0244b7d8ab9dbf2dbe00500945d773
kupi7u4 <deyes2016@gmail.com>
2022-05-26 12:04:49 +0300

    refactor: add const

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,13 +1,15 @@
 const fs = require('fs')
 const path = require('path')
+const templateFile = path.join(__dirname, 'template.html') 
+const componentsDirr = path.join(__dirname, 'components')
 const stylesDirr = path.join(__dirname, 'styles') 
 
 fs.promises.mkdir(path.join(__dirname, 'project-dist'), { recursive: true })
 
-fs.readFile(path.join(__dirname, 'template.html'), 'utf-8', (err, data) => {
+fs.readFile(templateFile, 'utf-8', (err, data) => {
   if (err) throw err
   let indexHtml = data
-  fs.readdir(path.join(__dirname, 'components'), (err, files) => {
+  fs.readdir(componentsDirr, (err, files) => {
     if (err) throw err
     files.forEach(el => {
       fs.readFile(path.join(__dirname, 'components', el), 'utf-8', (err, data) => {
